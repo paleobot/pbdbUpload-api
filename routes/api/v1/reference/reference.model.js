9b6c774f05f9b118ae9918669dceb29ac13e8dd0
Douglas Meredith <noisyfloweres@douglasmeredith.net>
2024-09-17 17:21:11 -0700

    Make createReference insert a refs record. There's some ghosted validation code that I will probably delete. Delete some ghost code

--- routes/api/v1/reference/reference.model.js
+++ routes/api/v1/reference/reference.model.js
@@ -1,3 +1,5 @@
+import {getPropertiesForPubType} from './reference.schema.js'
+
 export const getReferences = async (pool, limit, offset, fastify) => {
     //logger.info("getReferences");
     let conn;
@@ -47,7 +49,7 @@ export const getReference = async (pool, id) => {
     }
 }
 
-export const createReference = async (pool, reference, fastify) => {
+export const createReferencex = async (pool, reference, fastify) => {
     fastify.log.info("createReference");
     fastify.log.trace(reference);
     let conn;
@@ -66,4 +68,63 @@ export const createReference = async (pool, reference, fastify) => {
     } finally {
         if (conn) conn.release(); //release to pool
     }
+}
+
+export const createReference = async (pool, reference, userID, fastify) => {
+    fastify.log.info("createReference");
+    fastify.log.trace(reference);
+	
+    let properties = Object.keys(reference); //already validate by route logic
+    fastify.log.trace(properties)
+
+    const propMaster = getPropertiesForPubType(reference.publication_type, fastify)
+    fastify.log.trace(propMaster)
+
+	//Check for required properties (Arguably, we shouldn't do this)
+	//here. It's business logic and we don't care about that here.
+	//const propSet = new Set(properties);
+    //propMaster.requiredProps.forEach(prop => {
+	//	if (!propSet.has(prop)) {
+	//		throw new Error(`Missing required property: ${prop}`)
+	//	}
+    //})
+
+	let propStr = '';
+	let valStr = '';
+	const values = {};
+	properties.forEach((prop, index) => {
+		//Check if known property (We do need to do this here, though.
+		//A bad prop name would mess up our insert.)
+		//if (!propMaster.allowedProps.has(prop)) {
+		//	throw new Error(`Unrecognized propery: ${prop}`)
+		//}
+		propStr += index === 0 ? ` ${prop}` : `, ${prop}`;
+		valStr += index === 0 ? `:${prop}` : `, :${prop}`;
+        values[prop] = reference[prop]
+	})
+
+	propStr += `, enterer_no`;
+	valStr += `, :enterer_no`;
+    values.enterer_no = userID;
+    //TODO: Get pbdb.person.name for enterer
+
+    //TODO: Put this in transaction and update pbdb.person.last_action/last_entry(?)
+	const insertSQL = `insert into refs (${propStr}) values (${valStr})`
+	fastify.log.trace(insertSQL)
+	fastify.log.trace(values)
+
+    let conn;
+    try {
+        conn = await pool.getConnection();
+        const res = await conn.query({ 
+            namedPlaceholders: true, 
+            sql: insertSQL
+        }, values);
+        fastify.log.trace(res);
+        fastify.log.trace(res.affectedRows)
+        return res.affectedRows;
+    } finally {
+        if (conn) conn.release(); //release to pool
+    }
+  
 }

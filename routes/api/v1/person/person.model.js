d4c1a501fdb3d617c1bdf629b473699dd8a5de8f
Douglas Meredith <noisyfloweres@douglasmeredith.net>
2024-09-19 17:29:33 -0700

    Model for person. Not yet ready for use.

---
+++ routes/api/v1/person/person.model.js
@@ -0,0 +1,44 @@
+import {prepareInsertAssets} from '../../../../util.js'
+import {logger} from '../../../../app.js'
+
+export const createPerson = async (pool, person, user) => {
+    logger.info("createPerson");
+    logger.trace(person);
+
+    const insertAssets = prepareInsertAssets(reference);
+    //TODO: Other extra stuff for person? Similar to these for refs:
+	//insertAssets.propStr += `, enterer, enterer_no`;
+	//insertAssets.valStr += `, :enterer, :enterer_no`;
+    //insertAssets.values.enterer = user.userName;
+    //insertAssets.values.enterer_no = user.userID;
+
+	const insertSQL = `insert into person (${insertAssets.propStr}) values (${insertAssets.valStr})`
+	logger.trace(insertSQL)
+	logger.trace(insertAssets.values)
+
+    let conn;
+    try {
+        conn = await pool.getConnection();
+        await conn.beginTransaction();
+
+        //logger.trace("before update")
+        const rs = await conn.query("update person set last_action = now(), last_entry = now() where person_no = ?", [user.userID]);
+        if (rs.affectedRows !== 1) throw new Error("Could not update person table");
+
+        //logger.trace("before insert")
+        const res = await conn.query({ 
+            namedPlaceholders: true, 
+            sql: insertSQL
+        }, insertAssets.values);
+
+        //logger.trace("before commit")
+        await conn.commit();
+        return res;
+    } catch (err) {
+        logger.error("Error loading data, reverting changes: ", err);
+        await conn.rollback();
+    } finally {
+        if (conn) conn.release(); //release to pool
+    }
+  
+}

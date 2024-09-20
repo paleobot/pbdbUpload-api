a7ee3dbabec908a559631bc6354f102946580184
Douglas Meredith <noisyfloweres@douglasmeredith.net>
2024-09-19 17:30:03 -0700

    Add post endpoint. Stubbed for now (always success)

--- routes/api/v1/person/person.routes.js
+++ routes/api/v1/person/person.routes.js
@@ -2,4 +2,26 @@ export default async function (fastify, opts) {
     fastify.get('/', async function (request, reply) {
       return { msg: "person routes not yet implemented" }
     })
+
+    fastify.post(
+		'/',
+        {
+			preHandler : fastify.auth([
+				fastify.verifyAuth,
+			]),
+		  	//schema: schema
+		},
+		async (req, res) => {
+		  fastify.log.info("person POST")
+		  fastify.log.trace(req.body)
+  
+		  if (true/*await createPerson(fastify.mariadb, req.body.person, {userID: req.userID, userName: req.userName})*/) {
+			  //res.send('success');
+			  return {statusCode: 200, msg: "success"}
+		  } else {
+			  //res.send('failure');
+			  return {statusCode: 500, msg: "failure"}
+		  }
+	})
+
   }

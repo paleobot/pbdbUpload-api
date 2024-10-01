936dfba085e6384736864f60f20397d4ff8d1b25
Douglas Meredith <noisyfloweres@douglasmeredith.net>
2024-10-01 16:56:32 -0700

    Calculate derived values for lat/lon

--- routes/api/v1/collection/collection.model.js
+++ routes/api/v1/collection/collection.model.js
@@ -1,4 +1,4 @@
-import {prepareInsertAssets, prepareUpdateAssets} from '../../../../util.js'
+import {prepareInsertAssets, prepareUpdateAssets, calcDegreesMinutesSeconds} from '../../../../util.js'
 import {logger} from '../../../../app.js'
 
 export const getCollection = async (pool, id) => {
@@ -31,6 +31,24 @@ export const createCollection = async (pool, collection, user) => {
     insertAssets.values.enterer = user.userName; //TODO: consider stripping to first initial
     insertAssets.values.enterer_no = user.userID;
     insertAssets.values.authorizer_no = user.authorizerID;
+
+    //derived properties
+    insertAssets.propStr += `, latdir, latdeg, latmin, latsec, lngdir, lngdeg, lngmin, lngsec, coordinate`;
+    insertAssets.valStr += `, :latdir, :latdeg, :latmin, :latsec, :lngdir, :lngdeg, :lngmin, :lngsec, (PointFromText(:coordinate))`;
+    
+    const latDeg = calcDegreesMinutesSeconds(collection.lat)
+    insertAssets.values.latdir = collection.lat >= 0 ? "North" : "South";
+    insertAssets.values.latdeg = latDeg.degrees;
+    insertAssets.values.latmin = latDeg.minutes;
+    insertAssets.values.latsec = latDeg.seconds;
+    
+    const lonDeg = calcDegreesMinutesSeconds(collection.lng)
+    insertAssets.values.lngdir = collection.lng >= 0 ? "East" : "West";
+    insertAssets.values.lngdeg = lonDeg.degrees;
+    insertAssets.values.lngmin = lonDeg.minutes;
+    insertAssets.values.lngsec = lonDeg.seconds;
+
+    insertAssets.values.coordinate = `POINT(${collection.lat} ${collection.lng})`;
 
 	const insertSQL = `insert into collections (${insertAssets.propStr}) values (${insertAssets.valStr})`
 	logger.trace(insertSQL)
@@ -75,6 +93,24 @@ export const updateCollection = async (pool, patch, collectionID, user) => {
     updateAssets.values.modifier = user.userName; //TODO: consider stripping to first initial
     updateAssets.values.modifier_no = user.userID;
     updateAssets.values.collection_no = collectionID;
+
+    //derived properties
+    if (patch.lat || patch.lng) {}
+    updateAssets.propStr += `, latdir = :latdir, latdeg = :latdeg, latmin = :latmin, latsec = :latsec, lngdir = :lngdir, lngdeg = :lngdeg, lngmin = :lngmin, lngsec = :lngsec, coordinate = (PointFromText(:coordinate))`;
+    
+    const latDeg = calcDegreesMinutesSeconds(patch.lat)
+    updateAssets.values.latdir = patch.lat >= 0 ? "North" : "South";
+    updateAssets.values.latdeg = latDeg.degrees;
+    updateAssets.values.latmin = latDeg.minutes;
+    updateAssets.values.latsec = latDeg.seconds;
+    
+    const lonDeg = calcDegreesMinutesSeconds(patch.lng)
+    updateAssets.values.lngdir = patch.lng >= 0 ? "East" : "West";
+    updateAssets.values.lngdeg = lonDeg.degrees;
+    updateAssets.values.lngmin = lonDeg.minutes;
+    updateAssets.values.lngsec = lonDeg.seconds;
+
+    updateAssets.values.coordinate = `POINT(${patch.lat} ${patch.lng})`;
     
     const updateSQL = `update collections set ${updateAssets.propStr} where collection_no = :collection_no`
     logger.trace(updateSQL)

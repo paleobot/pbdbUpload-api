96a6c4312f7164be67265e5a51bab8517b7be571
Douglas Meredith <noisyfloweres@douglasmeredith.net>
2024-11-06 13:31:46 -0700

    Register swaggerUI here. Change return to link to help (swaggerUI)

--- routes/api/v1/v1.routes.js
+++ routes/api/v1/v1.routes.js
@@ -1,5 +1,41 @@
+import url from 'url'
+import {logger} from '../../../app.js'
+import swaggerUI from "@fastify/swagger-ui";
+import fs from 'fs'
+
 export default async function (fastify, opts) {
-  fastify.get('/', async function (request, reply) {
-    return { v1: true }
+
+	const image = fs.readFileSync('images/logo_grey.png', {encoding: 'base64'});
+	fastify.register(swaggerUI, {
+		//routePrefix is problematic.
+		//See https://github.com/fastify/fastify-swagger-ui/issues/180
+		//and many others.
+		routePrefix: "help",
+		logo: {
+			type: 'image/png',
+			content: Buffer.from(image, 'base64'),
+			//href: '/help',
+			target: '_blank'
+		},
+		theme: {
+			title: "PBDB upload API documentation",
+			favicon: [{
+				filename: 'logo_grey.png',
+				rel: 'icon',
+				sizes: '16x16',
+				type: 'image/png',
+				content: Buffer.from(image, 'base64')
+			}]
+		}
+		
+	});
+
+  fastify.get('/', async function (request, reply) {    
+    return { 
+        help: url.format({
+        protocol: request.protocol,
+        host: request.hostHack(),
+        pathname: request.url.replace(/\/$/, '') + "/help",
+    })}
   })
 }

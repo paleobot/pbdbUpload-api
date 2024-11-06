cd81fe5dbcf4920ee0ece1992821728536365305
Douglas Meredith <noisyfloweres@douglasmeredith.net>
2024-11-06 13:30:54 -0700

    Change to return link to v1

--- routes/api/api.routes.js
+++ routes/api/api.routes.js
@@ -1,5 +1,13 @@
+import url from 'url'
+import {logger} from '../../app.js'
+
 export default async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
-    return { api: true }
+    return { 
+    	v1: url.format({
+        protocol: request.protocol,
+        host: request.hostHack(),
+        pathname: request.url.replace(/\/$/, '') + "/v1",
+    })}
   })
 }
